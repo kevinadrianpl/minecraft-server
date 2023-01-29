32a7696c6cbc44089b5e7c5eaaad6adc0fed742b
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-01-29 18:50:42 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,18 +5,7 @@ import { ThemeSelect } from "@/components/ThemeSelect";
 const iconProps = { className: "w-4 h-4" };
 
 
-export const Footer: React.FC = () => {
-              <li key={key}>
-                <a
-                  href={href}
-                  className={cx(
-                    "w-8 h-8 grid place-items-center rounded-md",
-                    "bg-gray-900 text-gray-50",
-                    "dark:bg-gray-800 dark:text-gray-50"
-                  )}
-                  title={key}
-                >
-          })}
+
         </ul>
       ) : null}
       <div className="mt-8 flex justify-center">
