4971b2eb1e0c34c2e66bd54af353d6a58daab8fe
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-01-29 18:50:08 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,12 +6,6 @@ const iconProps = { className: "w-4 h-4" };
 
 
 export const Footer: React.FC = () => {
-  return (
-    <footer className="mt-auto py-8">
-      {siteConfig.social ? (
-        <ul className="flex justify-center space-x-4">
-          {Object.entries(siteConfig.social).map(([key, href]) => {
-            return (
               <li key={key}>
                 <a
                   href={href}
