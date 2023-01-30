ca0536a509abbea225c4cba65399d47c576d042e
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-01-30 20:48:47 +0200

    Update vote.tsx

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -1,27 +1,35 @@
 import type { NextPage } from "next";
+import { useEffect } from 'react';
 
 const Vote: NextPage = () => {
   return (
-    <>
-      <p>Please vote for our server at the following links:</p>
-      <ul style={{ paddingLeft: "20px" }}>
-        <li style={{ marginBottom: "10px" }}>
-          <a href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <a href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <a href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <a href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
-        </li>
-        <li style={{ marginBottom: "10px" }}>
-          <a href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
-        </li>
-      </ul>
-    </>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div>
+        <p style={{ textAlign: "center" }}>Please vote for us on the following servers:</p>
+        <br />
+        <ul style={{ textAlign: "center" }}>
+          <li>
+            <a style={{ color: "purple" }} href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
+          </li>
+          <br />
+          <li>
+            <a style={{ color: "purple" }} href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
+          </li>
+          <br />
+          <li>
+            <a style={{ color: "purple" }} href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
+          </li>
+          <br />
+          <li>
+            <a style={{ color: "purple" }} href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
+          </li>
+          <br />
+          <li>
+            <a style={{ color: "purple" }} href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
