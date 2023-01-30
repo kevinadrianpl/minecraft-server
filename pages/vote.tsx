8eeb7d692ea626db48a3f3050fdcf86a83528887
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-01-30 20:39:05 +0200

    Update vote.tsx

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -1,31 +1,24 @@
 import type { NextPage } from "next";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import "./vote.css";
 
 const Vote: NextPage = () => {
   return (
-    <div className="Vote">
-      <ul>
-        <li>
-          <a href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
-        </li>
-        <li>
-          <a href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
-        </li>
-        <li>
-          <a href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
-        </li>
-        <li>
-          <a href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
-        </li>
-      </ul>
-    </div>
+    <ul style={{ listStyle: "none", padding: 0, display: "flex", justifyContent: "space-around" }}>
+      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
+        <a href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
+      </li>
+      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
+        <a href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
+      </li>
+      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
+        <a href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
+      </li>
+      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
+        <a href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
+      </li>
+      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
+        <a href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
+      </li>
+    </ul>
   );
 };
 
