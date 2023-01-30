48d31f142cae64ce4e9b430fd1b282605a20deb2
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-01-30 22:05:07 +0200

    Update vote.tsx

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -1,35 +1,37 @@
+export default Vote;
+
 import type { NextPage } from "next";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Page } from "@/components/Page";
+import { Prose } from "@/components/Prose";
 
 const Vote: NextPage = () => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '50vh',
-      flexDirection: 'column',
-      textAlign: 'center'
-    }}>
-      <p>Hey! Help us get more players by casting your vote for the server using one of or all of the links below. Each vote gives you B500 in-game money and either 3 diamonds or 3 emeralds.</p>
-      <br />
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        <li>
-          <a style={{ color: 'purple' }} href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
-        </li>
-        <li>
-          <a style={{ color: 'purple' }} href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
-        </li>
-        <li>
-          <a style={{ color: 'purple' }} href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
-        </li>
-        <li>
-          <a style={{ color: 'purple' }} href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
-        </li>
-        <li>
-          <a style={{ color: 'purple' }} href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
-        </li>
-      </ul>
-    </div>
+    <Page
+      title="Voting Links"
+      description="List of links to vote for the server."
+    >
+      <Prose>
+        <ul style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <li>
+            <a style={{ color: "purple" }} href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
+          </li>
+          <li>
+            <a style={{ color: "purple" }} href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
+          </li>
+          <li>
+            <a style={{ color: "purple" }} href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
+          </li>
+          <li>
+            <a style={{ color: "purple" }} href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
+          </li>
+          <li>
+            <a style={{ color: "purple" }} href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
+          </li>
+        </ul>
+      </Prose>
+    </Page>
   );
 };
 
