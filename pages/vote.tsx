6076561a3daa9db5b884aeb8ac2368bfdd39475c
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-01-30 20:44:24 +0200

    Update vote.tsx

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -2,23 +2,26 @@ import type { NextPage } from "next";
 
 const Vote: NextPage = () => {
   return (
-    <ul style={{ listStyle: "none", padding: 0, display: "flex", justifyContent: "space-around" }}>
-      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
-        <a href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
-      </li>
-      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
-        <a href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
-      </li>
-      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
-        <a href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
-      </li>
-      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
-        <a href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
-      </li>
-      <li style={{ width: "20%", textAlign: "center", padding: "20px 0" }}>
-        <a href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
-      </li>
-    </ul>
+    <>
+      <p>Please vote for our server at the following links:</p>
+      <ul style={{ paddingLeft: "20px" }}>
+        <li style={{ marginBottom: "10px" }}>
+          <a href='https://minecraft-mp.com/server/221984/vote/'>Minecraft-MP.com</a>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <a href='https://www.planetminecraft.com/server/dxrery-networks/vote/'>PlanetMinecraft.com</a>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <a href='https://minecraftservers.org/vote/637524/'>MinecraftServers.org</a>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <a href='https://topminecraftservers.org/server/28267/'>TopMinecraftServers</a>
+        </li>
+        <li style={{ marginBottom: "10px" }}>
+          <a href='https://topg.org/minecraft-servers/server-535007'>TopG.org</a>
+        </li>
+      </ul>
+    </>
   );
 };
 
