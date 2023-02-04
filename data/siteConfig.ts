b5ae8958aecb78c3991f6a2c467985ab1765f607
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-02-04 20:05:28 +0200

    Update siteConfig.ts

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -8,10 +8,10 @@ const siteConfig: SiteConfig = {
   siteThumbnail: "/avatar.png",
   nav: [
     { label: "Posts", href: "/posts" },
-    { label: "Map", href: "/map" },
     { label: "About", href: "/about" },
-    { label: "Store", href: "/store" },
     { label: "Vote", href: "/vote" },
+    { label: "Map", href: "/map" },
+    { label: "Store", href: "/store" },
   ],
 };
 export default siteConfig;
