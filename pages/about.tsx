a659927b1d591af1d420a7f6f8efe8c1bc22281b
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-02-05 12:54:33 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,14 +15,12 @@ const About: NextPage = () => {
             We have many players from all over the world that play here but we mainly speak English on the server and Discord.
             We have mod-like features on the server including custom Quests, Jobs, special BattlePass, SlimeFun, DeathChests, Shops,
             Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, Towny, A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
-            This server is automated and runs on its own and everyone has a fair chance to make it big within the server.
           </p>
           <p>
             This server has a 16+ age restriction due to players swearing or the conversation is more adult orientated. 
             If you are under 16, you join at your own risk. As long as you don't mind a little language, it's mostly decent.
-            This is a small server and we don't mind. We would rather have 5 dedicated good players than 50 chaotic players. We will always 
-            remain small and niche, focusing on individuals rather than groups of bad people. We ban without warning if players are being 
-            chaotic or nasty to others. You have been warned!
+            We would rather have 5 dedicated good players than 50 chaotic players so we focus on individuals rather than groups. We ban 
+            without warning if players are being chaotic or nasty to others. You have been warned!
           </p>
         </Prose>
       </Page>
