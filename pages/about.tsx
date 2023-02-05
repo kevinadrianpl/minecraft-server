a32be09d90c1203d186c0590686f959f5ea6a90d
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-02-05 13:03:53 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,21 +11,18 @@ const About: NextPage = () => {
       >
         <Prose>
           <p>
-            Dxrery Networks is a small Minecraft community on a server currently hosted in 
-            Germany. We have many players from all over the world that play here but we 
-            mainly speak English on the server and Discord. We have mod-like features on 
-            the server including custom Quests, Jobs, special BattlePass, SlimeFun, 
-            DeathChests, Shops, Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, 
-            Towny, A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
+            Dxrery Networks is a Minecraft community on a server currently hosted in 
+            EU. We have players from all over that play here but we mainly speak 
+            English. We have mod-like features including Quests, Jobs, BattlePass, 
+            SlimeFun, DeathChests, Shops, Backpacks, PlayerWarps, Dynmap, PyroFishing, 
+            PyroMining, Towny, Resource World, SafariNet, and Leveled Mobs.
           </p>
           <p>
-            This server has a 16+ age restriction due to players swearing or the 
-            conversation is more adult orientated. If you are under 16, you join at 
-            your own risk. As long as you don't mind a little language, it's mostly 
-            decent. We would rather have 5 dedicated good players than 50 chaotic 
-            players so we focus on individuals rather than groups. We ban without 
-            warning if players are being chaotic or nasty to others. You have been 
-            warned!
+            This server is 16+ due to players swearing or the conversation is more 
+            adult orientated. If you are under 16, you join at your own risk. We would 
+            rather have 5 dedicated good players than 50 chaotic players so we focus 
+            on individuals rather than groups. We ban without warning if players are 
+            being chaotic or nasty to others. You have been warned!
           </p>
         </Prose>
       </Page>
