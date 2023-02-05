df24e2e6e88ea2264b7c98e4a6f8f24e4335c881
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-02-05 12:03:14 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,8 +15,14 @@ const About: NextPage = () => {
             currently hosted in Germany, but we occasionally move to SA.
             We have many players from all over the world that play on it,
             we mainly speak English on the server and Discord. Welcome.
-
-            
+          </p>
+          <p>
+            We have many mod-like features on the server, including custom
+            Quests, Jobs, special BattlePass, SlimeFun, DeathChests, Shops,
+            Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, Towny,
+            A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
+          </p>
+          <p>
             We have many mod-like features on the server, including custom
             Quests, Jobs, special BattlePass, SlimeFun, DeathChests, Shops,
             Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, Towny,
