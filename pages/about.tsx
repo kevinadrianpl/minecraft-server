2a32d954b33f03eda7500e831549158f7d9af040
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-02-05 12:13:56 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -23,16 +23,27 @@ const About: NextPage = () => {
             A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
           </p>
           <p>
-            We have many mod-like features on the server, including custom
-            Quests, Jobs, special BattlePass, SlimeFun, DeathChests, Shops,
-            Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, Towny,
-            A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
+            This server is mostly automated and runs on its own, staff
+            interference is very minimal. Everyone has a fair chance to
+            make it big within the server given enough time. Ranks allow
+            everyone the opportunity to fly and gain resources. We are NOT
+            pay to win. Everything can be earned with out timed ranks.
           </p>
           <p>
-            We have many mod-like features on the server, including custom
-            Quests, Jobs, special BattlePass, SlimeFun, DeathChests, Shops,
-            Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, Towny,
-            A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
+            This server has a 16+ age restriction due to the fact that
+            many players swear occasionally or the conversation is more
+            adult orientated. If you are under 16, you join at your own
+            risk. We are a family friendly server, as long as you don't
+            mind a little language here and there. It is mostly decent.
+          </p>
+          <p>
+            We realize this is a small server, we do not care. We would
+            rather have 5 dedicated players who are good people than 50
+            chaotic players. If you do not want to play because we are 
+            smaller than other servers, you are welcome to leave. We will
+            always remain small and niche. Focusing on individuals rather 
+            than groups of bad people. We ban without warning if players
+            are being chaotic or nasty to others. You have been warned!
           </p>
         </Prose>
       </Page>
