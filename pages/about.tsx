bce3590a37e9943afd8410d368be890ccb4d09c6
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2023-02-05 12:21:29 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,8 +13,6 @@ const About: NextPage = () => {
           <p>
             Dxrery Networks is a small Minecraft community on a server currently hosted in Germany, but we occasionally move to SA.
             We have many players from all over the world that play on it, we mainly speak English on the server and Discord. Welcome.
-          </p>
-          <p>
             We have many mod-like features on the server, including custom Quests, Jobs, special BattlePass, SlimeFun, DeathChests, Shops,
             Backpacks, PlayerWarps, Dynmap, PyroFishing, PyroMining, Towny, A resource world, SafariNets, Leveled Mobs, and Custom Terrain.
           </p>
@@ -22,8 +20,6 @@ const About: NextPage = () => {
             This server is mostly automated and runs on its own, staff interference is very minimal. Everyone has a fair chance to
             make it big within the server given enough time. Ranks allow everyone the opportunity to fly and gain resources. We are NOT
             pay to win. Everything can be earned with out timed ranks.
-          </p>
-          <p>
             This server has a 16+ age restriction due to the fact that many players swear occasionally or the conversation is more
             adult orientated. If you are under 16, you join at your own risk. We are a family friendly server, as long as you don't
             mind a little language here and there. It is mostly decent.
