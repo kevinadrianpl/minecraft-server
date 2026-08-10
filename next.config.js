49257c059e0bf5e1e6bed888f7c0818f5c331a67
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2026-08-10 17:24:37 +0200

    Remove @next/mdx usage: simplify next.config.js and remove @next/mdx from package.json so MDX is handled with next-mdx-remote (no Next MDX plugin required)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withMDX } = require('@next/mdx')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,4 +6,4 @@ const nextConfig = {
   }
 }
 
-module.exports = withMDX({ extension: /.mdx?$/ })(nextConfig)
+module.exports = nextConfig
