f3af08f03bb1d4dc0116d88f28ed9d6934691bc8
Kevin Adrian Pillay <30116824+kevinadrianpl@users.noreply.github.com>
2026-08-10 18:12:21 +0200

    Fix Prose props typing to accept children (React.ReactNode)

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,4 +1,10 @@
-export const Prose: React.FC = ({ children }) => {
+import React from "react";
+
+interface ProseProps {
+  children?: React.ReactNode;
+}
+
+export const Prose: React.FC<ProseProps> = ({ children }) => {
   return (
     <div className="max-w-none prose prose-a:text-pink-600 dark:prose-invert">
       {children}
